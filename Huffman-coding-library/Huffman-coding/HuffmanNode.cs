16a4c374da93a295629b4b4d16e941ac37143bce
Kurizaki <110892283+Kurizaki@users.noreply.github.com>
2024-03-15 10:54:44 +0100

    update

--- Huffman-coding-library/Huffman-coding/HuffmanNode.cs
+++ Huffman-coding-library/Huffman-coding/HuffmanNode.cs
@@ -1,12 +1,39 @@
-﻿namespace Huffman_coding
+﻿/*
+  ___  ___  ___  ___  ________ ________ _____ ______   ________  ________           ________  ________  ________  ___  ________   ________
+ |\  \|\  \|\  \|\  \|\  _____\\  _____\\   _ \  _   \|\   __  \|\   ___  \        |\   ____\|\   __  \|\   ___ \|\  \|\   ___  \|\   ____\
+ \ \  \\\  \ \  \\\  \ \  \__/\ \  \__/\ \  \\\__\ \  \ \  \|\  \ \  \\ \  \       \ \  \___|\ \  \|\  \ \  \_|\ \ \  \ \  \\ \  \ \  \___|
+  \ \   __  \ \  \\\  \ \   __\\ \   __\\ \  \\|__| \  \ \   __  \ \  \\ \  \       \ \  \    \ \  \\\  \ \  \ \\ \ \  \ \  \\ \  \ \  \  ___
+   \ \  \ \  \ \  \\\  \ \  \_| \ \  \_| \ \  \    \ \  \ \  \ \  \ \  \\ \  \       \ \  \____\ \  \\\  \ \  \_\\ \ \  \ \  \\ \  \ \  \|\  \
+    \ \__\ \__\ \_______\ \__\   \ \__\   \ \__\    \ \__\ \__\ \__\ \__\\ \__\       \ \_______\ \_______\ \_______\ \__\ \__\\ \__\ \_______\
+     \|__|\|__|\|_______|\|__|    \|__|    \|__|     \|__|\|__|\|__|\|__| \|__|        \|_______|\|_______|\|_______|\|__|\|__| \|__|\|_______|
+Authors: Keanu Koelewijn, Stefan Jesenko, Salma Tanner
+*/
+
+namespace Huffman_coding
 {
+    /// <summary>
+    /// Represents a node in a Huffman tree.
+    /// </summary>
     public class HuffmanNode
     {
+        /// <summary>
+        /// Gets or sets the character associated with the node.
+        /// </summary>
         public char Character { get; set; }
+
+        /// <summary>
+        /// Gets or sets the frequency of the character in the input data.
+        /// </summary>
         public int Frequency { get; set; }
+
+        /// <summary>
+        /// Gets or sets the left child node of the current node.
+        /// </summary>
         public HuffmanNode Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right child node of the current node.
+        /// </summary>
         public HuffmanNode Right { get; set; }
     }
-
-
 }
